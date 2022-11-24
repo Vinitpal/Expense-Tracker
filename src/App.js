5cf193c1cba74c1f915e2377096a11fc8ed2e4b4
Priyanshu Shrivastava <priyanshushri7@gmail.com>
2022-11-24 10:28:47 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 // import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Home  from "./pages/home";
+import Navbar from "./components/Navbar";
 import  Dashboard  from './pages/dashboard';
+<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+
 
 function App() {
   return (
     <>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Dashboard />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import "./Navbar.css";
+{/* <link rel="stylesheet" href="Navbar.css"> */}
 const Navbar = () => {
     return(
-        <div className=""></div>
+        // <div className=""></div>
+        <>
+        <div className="navbar">
+		    <div className="logo">
+			<a href="/">Expense Tracker</a>
+		</div>
+		<div className="nav_right">
+			<ul>
+				<li className="nr_li dd_main">
+					<img src="profile_pic.png" alt="profile_img" />
+				
+					<div className="dd_menu">
+						<div className="dd_left">
+							{/* <ul>
+								<li><i class="fas fa-map-marker-alt"></i></li>
+								<li><i class="far fa-star"></i></li>
+								<li><i class="far fa-plus-square"></i></li>
+								<li><i class="fas fa-cog"></i></li>
+								<li><i class="fas fa-download"></i></li>
+								<li><i class="fas fa-sign-out-alt"></i></li>
+							</ul> */}
+						</div>
+						<div className="dd_right">
+							<ul>
+								<li>Location</li>
+								<li>Favorites</li>
+								<li>Addpeople</li>
+								<li>Settings</li>
+								<li>Downloads</li>
+								<li>Logout</li>
+							</ul>
+						</div>
+					</div>
+				</li>
+				<li className="nr_li">
+					<i className="fas fa-envelope-open-text"></i>
+				</li>
+			</ul>
+		</div>
+	</div>
+        </>
     )
 }
+
+
+
+export default Navbar;
